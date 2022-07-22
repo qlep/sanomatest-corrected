727a8027fe5b562c05bd3bfdebbb05c1299732bf
Gleb Tishchenko <tishgle@digitalentshelsinki.internal>
2022-07-22 13:06:34 +0300

    pep8 mods

--- monitor.py
+++ monitor.py
@@ -1,29 +1,33 @@
-
+# Attempt to comply with PEP8
+# Standard modules, no need to include in requirements.
 import sys
 from datetime import datetime
 from time import sleep
+
+# Third party modules installed with pip.
 import requests
 
 checkstring = 'login'
 
-def pageLister():
-    '''reads from file list of urls, removes unnecessary newlines'''
-
-    urllist = []
-    urlstrings = []
+def page_lister():
+    '''Reads from file list of urls, removes unnecessary newlines,
+    returns list of urls.
+    '''
+    url_list = []
+    url_strings = []
 
     try:
-        urlfilename = sys.argv[1]
-        urlfile = open(urlfilename, 'r')
-        urlstrings = urlfile.readlines()
-        urlfile.close()
+        url_filename = sys.argv[1]
+        url_file = open(url_filename, 'r')
+        url_strings = url_file.readlines()
+        url_file.close()
 
-        for url in urlstrings:
+        for url in url_strings:
             url = url.replace('\n', '')
-            urllist.append(url)
+            url_list.append(url)
         
-        if len(urllist) != 0:
-            return urllist
+        if len(url_list) != 0:
+            return url_list
         else:
             print('File is empty!')
             return None
@@ -31,26 +35,32 @@ def pageLister():
         print('No file name given!')
         return None
 
-def responseGetter(url):
-    '''takes list of urls as parameter, makes page requests, returns logs'''
-    
+def response_getter(url):
+    '''Takes list of urls, makes page requests,
+    returns log strings.
+    '''
     resultstring = ''
 
     try:
         response = requests.get(url)
-        resultstring = contentChecker(checkstring, response)
+        resultstring = content_checker(checkstring, response)
     except requests.exceptions.ConnectionError:
-        resultstring = '{0} {1}: page does not exsist\n'.format(datetime.now(), url)
+        resultstring = '{0} {1}: page does not exsist\n'.format(
+            datetime.now(), url)
     except:
-        resultstring = '{0} {1}: incorrect URL format\n'.format(datetime.now(), url)
+        resultstring = '{0} {1}: incorrect URL format\n'.format(
+            datetime.now(), url)
 
     return resultstring
 
-def contentChecker(checkstring, content):
-    '''takes content of requested pages and checkstring as parameters, performes checks, returns result strings'''
-    
+def content_checker(checkstring, content):
+    '''Takes content of requested pages and checkstring as parameters,
+    performes checks, returns result strings.
+    '''
     content = content
-    resultstring = '{0} {1}: Status: {2} --- Response time:{3}s'.format(datetime.now(), content.url, content.status_code, content.elapsed)
+    resultstring = '{0} {1}: Status: {2} --- Response time:{3}s'.format(
+        datetime.now(), content.url, 
+        content.status_code, content.elapsed)
 
     if content.status_code == 200:
         if checkstring in content.text:
@@ -60,16 +70,15 @@ def contentChecker(checkstring, content):
     else:
         return resultstring + '\n'
 
-def processLooper(urllist):
-    '''loops process, writes logs to file, breaks on empty file'''
-
-    urllist = urllist
+def process_looper(url_list):
+    '''Loops process, writes logs to file, breaks on empty file.'''
+    url_list = url_list
     
-    if urllist:
+    if url_list:
         while True:
-            for url in urllist:
+            for url in url_list:
                 logs = open('logs.txt', 'a')
-                log = responseGetter(url)
+                log = response_getter(url)
                 print(log)
                 logs.write(log)
                 logs.close()
@@ -79,4 +88,4 @@ def processLooper(urllist):
             except IndexError:
                 sleep(5)
 
-processLooper(pageLister())
+process_looper(page_lister())
